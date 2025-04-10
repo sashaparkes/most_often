9e3245ed0f005f802601f076f7557a2f2baa0096
Jordan <JordanGill@live.co.uk>
2025-04-10 15:29:40 +0100

    Jordan first commit

--- tests/test_most_often.py
+++ tests/test_most_often.py
@@ -26,3 +26,25 @@ def test_unique_items_draw():
 def test_unique_items_empty_list():
     example = MostOften([])
     assert example.get_most_often() == "no clear winner"
+
+def test_unique_items_one_duplicate():
+    example = MostOften(["apple", "banana", "pineapple", "apple", "kiwi", "apple"])
+    assert example.get_most_often() == "apple"
+
+def test_unique_items_two_duplicates():
+    example = MostOften(["apple", "banana", "pineapple", "apple", "kiwi", "apple", "kiwi", "kiwi"])
+    assert example.get_most_often() == "no clear winner"
+
+def test_unique_items_add_item():
+    example = MostOften(["apple", "banana", "pineapple", "apple", "kiwi", "apple", "kiwi", "kiwi"])
+    example.add_new("apple")
+    assert example.get_most_often() == "apple"
+
+def test_unique_items_add_items():
+    example = MostOften(["apple", "banana", "pineapple", "apple", "kiwi", "apple", "kiwi", "kiwi"])
+    example.add_new("kiwi")
+    example.add_new("pineapple")
+    example.add_new("pineapple")
+    example.add_new("pineapple")
+    example.add_new("pineapple")
+    assert example.get_most_often() == "pineapple"
